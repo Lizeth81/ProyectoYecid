135d2f624109fd99c708540c1a7253cd56bf97f4
LizethPrado <lizethprado81@gmail.com>
2023-04-18 23:03:55 -0500

    Se actualiza el inicio de pantalla

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import '../src/css/login.css';
-import { Button, Divider, Form, Input, Typography } from 'antd';
+import { Button, Divider, Form, Input, Typography, message } from 'antd';
 //const ImgPrincipal = require.context('../src/assets', true);
 
 function App() {
+
+  const login=()=>{
+    message.success('¡Inicio de sesión exitosa!');
+  };
   return (
     /*<img src={ImgPrincipal('./fondo-udes.jpg')} className="" alt="logo" />*/
     <div className="init">
-       <Form className="loginForm">
-        <Typography.Title style={{textAlign: "center"}}>Iniciar Sesión</Typography.Title>
-        <Form.Item label="Usuario" name={"MyEmail"}>
+       <Form className="loginForm" onFinish={login}>
+        <Typography.Title style={{textAlign: "center"}} >Iniciar Sesión</Typography.Title>
+        <Form.Item rules={[
+          {
+            required: true, 
+            message: "Ingresar un correo electronico"
+            }
+          ]}label="Usuario" name={"MyEmail"}>
           <Input placeholder="Usuario"/>
         </Form.Item>
-        <Form.Item label="Contraseña" name={"MyPassword"}>
-          <Input placeholder="**********"/>
+        <Form.Item rules={[
+          {
+            required: true, 
+            message: "Ingresar una contraseña"
+            }
+          ]} label="Contraseña" name={"MyPassword"}>
+          <Input.Password placeholder="**********"/>
         </Form.Item>
         <Button type="primary" htmlType="submit" block>Aceptar</Button>
         <br/>
